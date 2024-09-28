3ada5e1199874e56902e4475dd5e7073cf03d1fc
heaseung123 <showui96@gmail.com>
2024-09-28 17:36:55 +0900

    chore: url 변경
    develop

--- src/components/Chat/useFetchChatLogs.ts
+++ src/components/Chat/useFetchChatLogs.ts
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 
 // Redis에서 조회하기
 const fetchMessagesFromRedis = async (chatRoomId: string) => {
-	const response = await fetch(`http://lingochat-ai.io/api/chats/redis/${chatRoomId}`, {
+	const response = await fetch(`/api/chats/redis/${chatRoomId}`, {
 		method: 'GET',
 		headers: {
 			'Content-Type': 'application/json',
@@ -20,7 +20,7 @@ const fetchMessagesFromRedis = async (chatRoomId: string) => {
 
 // DB에서 조회하기
 const fetchMessagesFromDB = async (chatRoomId: string) => {
-	const response = await fetch(`http://lingochat-ai.io/api/chats/db/${chatRoomId}`, {
+	const response = await fetch(`/api/chats/db/${chatRoomId}`, {
 		method: 'GET',
 		headers: {
 			'Content-Type': 'application/json',
